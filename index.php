c5916d14cd53fc1605c13b874927c030f1771bf0
kan zhang <kz233@afsconnect1.njit.edu>
2017-10-15 20:29:51 -0400

    Quick finished

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 'On');
+error_reporting(E_ALL | E_STRICT);
+
 header("Cache-Control: no-cache, must-revalidate");
 header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");
 
@@ -8,6 +11,8 @@ header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");
 //header("Content-Disposition:attachment;filename='Downloads.csv'");
 // The PDF source is in original.pdf
 //readfile("C.csv");
+
+
 ?>
 
 <!DOCTYPE html>
@@ -19,6 +24,17 @@ header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");
     <input type="file" name="fileToUpload" id="fileToUpload">
     <input type="submit" value="Upload Image" name="submit">
 </form>
+
+<?php
+$dir    = 'CSVdata';
+$files = scandir($dir);
+//for($i=2;$i<count($files[]);$i++){
+
+//echo "</br>";
+print_r($files);
+
+//class basic
+?>
 
 </body>
 </html>
--- upload.php
+++ upload.php
@@ -1,46 +1,78 @@
 <?php
+ini_set('display_errors', 'On');        //Debug messages
+error_reporting(E_ALL | E_STRICT);
+
+header("Cache-Control: no-cache, must-revalidate");     //Don't use cache
+header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");
+
 $target_dir = "CSVdata/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
-$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+//$CSVFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+$appp =file($_FILES["fileToUpload"]["tmp_name"]);
+
+echo "<html>";
+echo "<body>";
+echo "<table style='width:100%'>";
+for($i=0; $i<count($appp); $i++){
+	echo "<tr>";
+	foreach(explode(",", $appp[4]) as $j) {
+	echo "<td>$j</td>";
+	}
+	echo "</tr>";
+}
+echo "</table>";
+echo "</body>";
+echo "</html>";
+
+//print_r($appp);
+//echo count($appp);
+
+//echo ( $fgetcsv($_FILES["fileToUpload"]["tmp_name"]));
+//print_r($content);
+
+//var_dump($content);
 // Check if image file is a actual image or fake image
 
-if(isset($_POST["submit"])) {
-	$check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-	if($check !== false) {
-		echo "File is an image - " . $check["mime"] . ".";
-		$uploadOk = 1;
-	} else {
-		echo "File is not an image.";
-		$uploadOk = 0;
-	}
-}
+//if(isset($_POST["submit"])) {
+//	$check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+//	if($check !== false) {
+//		echo "File is an image - " . $check["mime"] . ".";
+//		$uploadOk = 1;
+//	} else {
+//		echo "File is not an image.";
+//		$uploadOk = 0;
+//	}
+//}
+
 // Check if file already exists
-if (file_exists($target_file)) {
-	echo "Sorry, file already exists.";
-	$uploadOk = 0;
-}
-// Check file size
-if ($_FILES["fileToUpload"]["size"] > 500000) {
-	echo "Sorry, your file is too large.";
-	$uploadOk = 0;
-}
-// Allow certain file formats
-if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif" ) {
-	echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-	$uploadOk = 0;
-}
+//if (file_exists($target_file)) {
+//	echo "Sorry, file already exists.";
+//	$uploadOk = 0;
+//}
+//// Check file size
+//if ($_FILES["fileToUpload"]["size"] > 500000) {
+//	echo "Sorry, your file is too large.";
+//	$uploadOk = 0;
+//}
+//// Allow certain file formats
+//if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif" ) {
+//	echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+//	$uploadOk = 0;
+//}
+
 // Check if $uploadOk is set to 0 by an error
-if ($uploadOk == 0) {
-	echo "Sorry, your file was not uploaded.";
-	// if everything is ok, try to upload file
-} else {
-	if(move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-	echo "The file ". basename($_FILES["fileToUpload"]["name"]). " has been uploaded.";
-	} else {
-	echo "Sorry, there was an error uploading your file.";
-	}
-}
+//if ($uploadOk == 0) {
+//	echo "Sorry, your file was not uploaded.";
+//	// if everything is ok, try to upload file
+//} else {
+//	if(move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+//	echo "The file ". basename($_FILES["fileToUpload"]["name"]). " has been uploaded.";
+//	} else {
+//	echo "Sorry, there was an error uploading your file.";
+//	}
+//}
+
 //header("Content-type:application/jpg");
 // It will be called downloaded.pdf
 //header("Content-Disposition:attachment;filename=download.jpg");
@@ -48,5 +80,3 @@ if ($uploadOk == 0) {
 //readfile($target_file);
 
 ?>
-</br>
-<img src="<?php echo $target_file; ?>">
