aa31f94ac1a101c5ace75c58c5e87e5c09b98817
kan zhang <kz233@afsconnect1.njit.edu>
2017-10-15 23:50:34 -0400

    Changed logic

--- index.php
+++ index.php
@@ -11,7 +11,11 @@ header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");
 //header("Content-Disposition:attachment;filename='Downloads.csv'");
 // The PDF source is in original.pdf
 //readfile("C.csv");
-
+if(isset($_GET["wn"])){
+	if($_GET["wn"] = 1){
+	echo "PLEASE CHOOSE FILE!!!";	
+	}
+}
 
 ?>
 
@@ -20,17 +24,19 @@ header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");
 <body>
 
 <form action="upload.php" method="post" enctype="multipart/form-data">
-    Select image to upload:
+    Select CSV to upload:
     <input type="file" name="fileToUpload" id="fileToUpload">
-    <input type="submit" value="Upload Image" name="submit">
+    <input type="submit" value="Click HERE TO Upload" name="submit">
 </form>
 
-<?php
-$dir    = 'CSVdata';
-$files = scandir($dir);
-//for($i=2;$i<count($files[]);$i++){
 
-//echo "</br>";
+
+<?php
+$files = scandir('CSVdata');
+echo "EXISTING FILEs:";
+for($i = 2; $i < count($files); $i++){
+	echo "</br>" . ($i-1) . ". "  . $files[$i];
+}
 //print_r($files);
 
 //class basic
--- upload.php
+++ upload.php
@@ -4,23 +4,16 @@ error_reporting(E_ALL | E_STRICT);
 
 header("Cache-Control: no-cache, must-revalidate");     //Don't use cache
 header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");
+//var_dump( $_FILES["fileToUpload"]["name"]);
+if(!isset($_POST["submit"]) or $_FILES["fileToUpload"]["name"] == "") {
+	header('Location: index.php?wn=1');
+}
+
 
 $target_dir = "CSVdata/";
 $target_file = $target_dir . $_FILES["fileToUpload"]["name"];
 $uploadOk = 1;
 $appp =file($_FILES["fileToUpload"]["tmp_name"]);
-
-
-//if(isset($_POST["submit"])) {
-//	$check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-//	if($check !== false) {
-//		echo "File is an image - " . $check["mime"] . ".";
-//		$uploadOk = 1;
-//	} else {
-//		echo "File is not an image.";
-//		$uploadOk = 0;
-//	}
-//}
 
 // Check file exists
 if (file_exists($target_file)) {
